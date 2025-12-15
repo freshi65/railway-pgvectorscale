346bf6f2bcc6720c7ab80b7e6c3fe3be001752c7
freshi65 <leon.maassen98@gmail.com>
2025-12-15 17:59:10 +0100

    test

--- entrypoint-wrapper.sh
+++ entrypoint-wrapper.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 set -e
 
-# Fix permissions on the mounted volume (runs as root)
-mkdir -p /home/postgres/pgdata/data
+PGDATA="/home/postgres/pgdata/data"
+
+# Fix permissions
+mkdir -p "$PGDATA"
 chown -R postgres:postgres /home/postgres/pgdata
 
-# Switch to postgres user and run original entrypoint
-exec gosu postgres /usr/local/bin/docker-entrypoint.sh "$@"
+# Initialize if needed
+if [ ! -s "$PGDATA/PG_VERSION" ]; then
+    su postgres -c "/usr/lib/postgresql/17/bin/initdb -D $PGDATA"
+    
+    # Configure pg_hba.conf for connections
+    echo "host all all 0.0.0.0/0 md5" >> "$PGDATA/pg_hba.conf"
+    echo "listen_addresses='*'" >> "$PGDATA/postgresql.conf"
+    
+    # Start temporarily to create user/db
+    su postgres -c "/usr/lib/postgresql/17/bin/pg_ctl -D $PGDATA start"
+    su postgres -c "psql -c \"ALTER USER postgres WITH PASSWORD '${POSTGRES_PASSWORD}';\""
+    su postgres -c "psql -c \"CREATE DATABASE ${POSTGRES_DB:-railway};\""
+    su postgres -c "/usr/lib/postgresql/17/bin/pg_ctl -D $PGDATA stop"
+fi
+
+# Start postgres
+exec su postgres -c "/usr/lib/postgresql/17/bin/postgres -D $PGDATA"
